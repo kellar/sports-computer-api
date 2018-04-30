d3205bd025b2af07d7c5a8321a0db4abb27fc201
kellar <blakekellar@gmail.com>
2018-04-29 22:07:27 -0500

    start.spring.io generated project

---
+++ src/main/kotlin/com/blakekellar/sportscomputer/sportscomputerapi/SportsComputerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.blakekellar.sportscomputer.sportscomputerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SportsComputerApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SportsComputerApiApplication>(*args)
+}
