465640ca323975544a60a98f30ee3f74e743b650
kellar <blakekellar@gmail.com>
2018-05-10 20:57:04 -0500

    Adds test case

--- src/test/kotlin/com/blakekellar/sportscomputer/SportscomputerApplicationTests.kt
+++ src/test/kotlin/com/blakekellar/sportscomputer/SportscomputerApplicationTests.kt
@@ -13,7 +13,10 @@ import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.body
 import org.springframework.test.web.reactive.server.returnResult
 import reactor.core.publisher.Flux
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.*
+
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,7 +25,7 @@ class SportscomputerApplicationTests {
     companion object : KLogging()
 
     @Autowired private lateinit var webClient: WebTestClient
-
+    /
     @Test
     fun emptyPostBodyIsBadRequest() {
         this.webClient.post().uri("/gameresults").exchange().expectStatus().isBadRequest
@@ -87,12 +90,11 @@ class SportscomputerApplicationTests {
     }
 
     @Test
-    @Ignore // TODO:  Fails with org.apache.commons.math3.linear.SingularMatrixException: matrix is singular
-    fun computesSrsForLargerRequest() {
+    fun computesSrsForLargeRequest() {
         val gameResults: MutableList<GameResult> = mutableListOf()
 
         // construct a set of game results
-        for (i in 0..100) {
+        for (i in 0..10000) {
             // construct two teams "teamM" and "teamN" with random scores
             val team1 = randomBetween(0, 9)
             val team2 = randomBetween(0, 9, team1)
@@ -108,6 +110,128 @@ class SportscomputerApplicationTests {
 
         val response = this.webClient.post().uri("/gameresults").body(postBody).exchange()
         response.expectStatus().isOk
-        logger.info("request=${gameResults} response=${response.returnResult<String>()}")
     }
+
+    @Test
+    @Ignore // TODO:  Fails with org.apache.commons.math3.linear.SingularMatrixException: matrix is singular
+    fun computesSrsForSmallRequest() {
+        val gameResults: MutableList<GameResult> = mutableListOf()
+
+        // construct a set of game results
+        for (i in 0..30) {
+            // construct two teams "teamM" and "teamN" with random scores
+            val team1 = randomBetween(0, 9)
+            val team2 = randomBetween(0, 9, team1)
+            val team1score = randomBetween(0.0, 100.0)
+            val team2score = randomBetween(0.0, 100.0)
+            gameResults.add(gameResultFactory("team" + team1, team1score, "team" + team2, team2score))
+        }
+
+        logger.info("request=${gameResults}")
+
+        var postBody: Flux<GameResult> = Flux.empty()
+        gameResults.forEach { gameResult ->
+            postBody = postBody.concatWith(Flux.just(gameResult))
+        }
+
+        val response = this.webClient.post().uri("/gameresults").body(postBody).exchange()
+        response.expectStatus().isOk
+        response.returnResult<String>().responseBody.subscribe({
+
+            logger.info("response=${response.returnResult<String>()}")
+        })
+        Thread.sleep(1000)
+    }
+
+    @Test
+    @Ignore // TODO:  Fails with org.apache.commons.math3.linear.SingularMatrixException: matrix is singular
+    fun computesSrsKnownResult() {
+
+        //https://codeandfootball.wordpress.com/2011/04/12/issues-with-the-simple-ranking-system/
+
+        val gameResults: MutableList<GameResult> = mutableListOf()
+        gameResults.add(gameResultFactory("A", 27.0, "B", 13.0))
+        gameResults.add(gameResultFactory("A", 15.0, "C", 3.0))
+        gameResults.add(gameResultFactory("A", 7.0, "D", 19.0))
+        gameResults.add(gameResultFactory("A", 35.0, "E", 20.0))
+
+        gameResults.add(gameResultFactory("B", 13.0, "A", 27.0))
+        gameResults.add(gameResultFactory("B", 24.0, "E", 27.0))
+        gameResults.add(gameResultFactory("B", 9.0, "F", 31.0))
+        gameResults.add(gameResultFactory("B", 18.0, "D", 20.0))
+
+        gameResults.add(gameResultFactory("C", 21.0, "D", 17.0))
+        gameResults.add(gameResultFactory("C", 3.0, "C", 15.0))
+        gameResults.add(gameResultFactory("C", 10.0, "E", 30.0))
+        gameResults.add(gameResultFactory("C", 12.0, "F", 15.0))
+
+        gameResults.add(gameResultFactory("D", 17.0, "C", 21.0))
+        gameResults.add(gameResultFactory("D", 7.0, "F", 17.0))
+        gameResults.add(gameResultFactory("D", 19.0, "A", 7.0))
+        gameResults.add(gameResultFactory("D", 20.0, "B", 18.0))
+
+        gameResults.add(gameResultFactory("E", 30.0, "F", 41.0))
+        gameResults.add(gameResultFactory("E", 21.0, "B", 24.0))
+        gameResults.add(gameResultFactory("E", 30.0, "C", 10.0))
+        gameResults.add(gameResultFactory("E", 20.0, "A", 35.0))
+
+        gameResults.add(gameResultFactory("F", 41.0, "E", 30.0))
+        gameResults.add(gameResultFactory("F", 17.0, "D", 7.0))
+        gameResults.add(gameResultFactory("F", 31.0, "B", 9.0))
+        gameResults.add(gameResultFactory("F", 15.0, "C", 12.0))
+
+        logger.info("request=${gameResults}")
+
+        var postBody: Flux<GameResult> = Flux.empty()
+        gameResults.forEach { gameResult ->
+            postBody = postBody.concatWith(Flux.just(gameResult))
+        }
+
+        val response = this.webClient.post().uri("/gameresults").body(postBody).exchange()
+        response.expectStatus().isOk
+
+        /*
+        Expected:
+        A 2.93
+        B -7.19
+        C -6.19
+        D -0.82
+        E -3.07
+        F 7.18
+         */
+
+        logger.info("response=${response.returnResult<String>()}")
+    }
+
+    @Test
+    fun foo() {
+        val path = Paths.get(javaClass.classLoader.getResource("2017.csv")!!.toURI())
+        val gameResults: MutableList<GameResult> = mutableListOf()
+        val lines = Files.lines(path)
+
+        var count = 0
+        lines.forEach { line ->
+            count++
+
+            if (count < 201) {
+                // TODO: RFC4180 parser e.g. jackson
+                var teamScoreTeamScore = line.split(',')
+                val gameResult = gameResultFactory(teamScoreTeamScore[0], teamScoreTeamScore[1].toDouble(), teamScoreTeamScore[2], teamScoreTeamScore[3].toDouble())
+                logger.info("Adding " + gameResult)
+                gameResults.add(gameResult)
+
+            }
+        }
+
+        logger.info("request=${gameResults}")
+
+        var postBody: Flux<GameResult> = Flux.empty()
+        gameResults.forEach { gameResult ->
+            postBody = postBody.concatWith(Flux.just(gameResult))
+        }
+
+        this.webClient.post().uri("/gameresults").body(postBody).exchange().returnResult(String::class.java).responseBody.subscribe({ a -> println("a=" + a) })
+    }
+
 }
+
